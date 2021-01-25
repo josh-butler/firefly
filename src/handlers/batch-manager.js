582bef50b829cdceb4a2f68e4c1f3bae024bcb5b
josh-butler <scinewave@gmail.com>
2021-01-24 21:53:45 -0500

    query batches

--- src/handlers/batch-manager.js
+++ src/handlers/batch-manager.js
@@ -1,8 +1,48 @@
-exports.handler = async event => {
-  console.log('event: ', event);
-  const statusCode = 200;
-  const headers = { 'Access-Control-Allow-Origin': '*', 'Content-Type': 'application/json' };
-  const body = JSON.stringify({ status: 'ok' });
+const { EntityTable } = require('../ddb');
 
-  return { statusCode, headers, body };
+class BatchManager {
+  constructor(status) {
+    this.status = status;
+    this.props = this.defaultProps();
+  }
+
+  defaultProps() {
+    const prefix = 'BATCH#';
+    const pk = this.status;
+
+    return { pk, prefix };
+  }
+
+  async getBatches() {
+    let err;
+    let result;
+    const { pk, prefix } = this.props;
+
+    try {
+      result = await EntityTable.query(
+        pk,
+        { beginsWith: prefix, index: 'GSI1' },
+      );
+    } catch (e) {
+      console.error(e);
+      err = e;
+    }
+
+    // if (result) {
+    //   const { Items = [] } = result;
+    //   const sorted = Items.sort((a, b) => b.modified - a.modified);
+    //   data = sorted.map(batchAttrs);
+    // }
+
+    return { result, err };
+  }
+
+  async send() {
+    return this.getBatches();
+  }
+}
+
+exports.handler = async () => {
+  const manager = new BatchManager('QUEUED');
+  return manager.send();
 };
