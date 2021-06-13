b0a51f3bfd578035e3d6d19b659a7b00c2a2d779
josh-butler <scinewave@gmail.com>
2021-06-13 19:32:21 -0400

    add attrs to classes

--- src/handlers/batch-manager.js
+++ src/handlers/batch-manager.js
@@ -1,93 +1,33 @@
 /* eslint-disable max-classes-per-file */
 const { EntityTable, Batch: BatchEntity, Job: JobEntity } = require('../util/ddb');
-// const { postBatonRequest } = require('../utils/lambda');
-// const { logBase64 } = require('../utils/util');
 const { publishEvent } = require('../util/events');
-
 const { BATCH_LIMIT } = require('../config');
 
 const logInfo = (message, params = {}) => {
-  const base = { type: 'BatchJobSubmit', message };
+  const base = { type: 'BatchManager', message };
   console.info(JSON.stringify({ ...base, ...params }));
 };
-
-const submittedParams = ({ data: { pk, sk } }) => ({ pk, sk, status: 'ACCEPTED' });
-
-const batchAttrs = ({
-  pk, id, eid,
-}) => ({
-  pk, id, eid,
-});
-
-const jobAttrs = ({
-  pk, sk, path, plan,
-}) => ({
-  pk, sk, path, plan,
-});
-
-// async getOrder() {
-//   let err;
-//   let res;
-//   const { orderId: id } = this.props;
-
-//   try {
-//     const result = await queryOrdersInterface({ type: 'getOrder', params: { id } });
-//     const { Payload } = result;
-//     res = Payload;
-//   } catch (e) {
-//     console.error(e);
-//     err = e;
-//   }
-
-//   if (res && !err) {
-//     const { data, err } = JSON.parse(res);
-//     err = error;
-//     this.order = data;
-//   }
-
-//   return err;
-// }
-
-// const getBatonTasks = async () => {
-//   let err;
-
-//   const res = await postBatonRequest({ type: 'listTasks', params: {} });
-//   const { LogResult, Payload } = res;
-
-//   if (Payload === 'null') {
-//     err = 'translator message failed';
-//     logBase64(LogResult);
-//   }
-
-//   // return err ? resp500({ message: err }) : null
-//   return err ? resp500({ message: err }) : null
-// }
 
 class Job {
   constructor(data) {
     this.data = data;
-    this.props = this.defaultProps();
   }
 
-  defaultProps() {
-    const { plan, path } = this.data;
-    return { plan, path };
+  get putParams() {
+    const { pk, sk } = this.data;
+    return { pk, sk, status: 'ACCEPTED' };
   }
 
-  async submit() {
-    const { plan, path } = this.props;
-    logInfo('submitting job', { plan, path });
+  get attrs() {
+    const {
+      pk, sk, path, plan,
+    } = this.data;
 
-    // TODO:
-    // - [ ] submit VerifyFile job to Baton (via Lambda invoke?)
-    // - [ ] store return taskId on Job
-    // - [ ] throw if error
-    // - [ ] consder Q for retries? since Baton is not reliable
-
-    return null;
+    return {
+      pk, sk, path, plan,
+    };
   }
 }
-
 
 class Batch {
   constructor(data) {
@@ -99,6 +39,11 @@ class Batch {
     const { pk } = this.data;
     const status = 'ACCEPTED';
     return { pk, status };
+  }
+
+  get attrs() {
+    const { pk, id, eid } = this.data;
+    return { pk, id, eid };
   }
 
   async getJobItems() {
@@ -116,14 +61,13 @@ class Batch {
     await this.getJobItems();
     this.jobs = this.items.map(item => new Job(item));
 
-    this.payload = { ...batchAttrs(this.data), jobs: this.jobs.map(({ data }) => jobAttrs(data)) };
-    console.log('this.payload: ', this.payload);
-
     // update Batch status to ACCEPTED
     await BatchEntity.update({ pk, sk: pk, status });
 
     // update Jobs status to ACCEPTED
-    await Promise.all(this.jobs.map(job => JobEntity.update(submittedParams(job))));
+    await Promise.all(this.jobs.map(job => JobEntity.update(job.putParams)));
+
+    this.payload = { ...this.attrs, jobs: this.jobs.map(job => job.attrs) };
 
     return publishEvent('SUBMIT_BATCH', this.payload);
   }
