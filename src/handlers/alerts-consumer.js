d1ee2cac60aec03020e01a36a036e1656c530288
josh-butler <scinewave@gmail.com>
2021-06-27 20:44:38 -0400

    add complete job class

--- src/handlers/alerts-consumer.js
+++ src/handlers/alerts-consumer.js
@@ -1,8 +1,6 @@
-/* eslint-disable max-classes-per-file */
 const xml2js = require('xml2js');
 
-const { taskReport } = require('../util/lambda');
-const { uploadReport } = require('../util/s3');
+const { CompletedJob } = require('../util/job');
 
 const logMsg = (message, params = {}) => {
   const base = { type: 'BatonAlert', message };
@@ -26,68 +24,6 @@ const parseAlerts = async xmls => {
   const alerts = await Promise.all(xmls.map(parseAlertXml));
   return alerts.filter(i => i);
 };
-
-const getTaskReport = async taskId => {
-  let err;
-  let res;
-  let data;
-
-  try {
-    const result = await taskReport({ type: 'taskReport', params: { taskId } });
-    const { Payload } = result;
-    res = Payload;
-  } catch (e) {
-    console.error(e);
-    err = e.message;
-  }
-
-  if (res && !err) {
-    const { data: d, error } = JSON.parse(res);
-    err = error;
-    data = d;
-  }
-
-  return { data, err };
-};
-
-class CompletedJob {
-  constructor(event) {
-    this.event = event;
-    this.props = this.defaultProps();
-  }
-
-  defaultProps() {
-    const { contentId, taskId } = this.event;
-    const [pk, sk] = contentId.split('|');
-    return {
-      pk, sk, contentId, taskId,
-    };
-  }
-
-  async getReport() {
-    const { taskId } = this.props;
-    const { data, err } = await getTaskReport(taskId);
-    this.report = data;
-    return err;
-  }
-
-  async uploadReport() {
-    const { taskId } = this.props;
-    return uploadReport(`${taskId}.xml`, this.report);
-  }
-
-  async process() {
-    const err = await this.getReport();
-
-    // TODO handle error, updated ddb as failed & send failed event
-    if (!err) {
-      const res = await this.uploadReport();
-      console.log('res: ', res);
-    }
-    console.log('err: ', err);
-    console.log('this.report: ', this.report);
-  }
-}
 
 const handleTaskCompleted = async alert => {
   const job = new CompletedJob(alert);
---
+++ src/util/job.js
@@ -0,0 +1,77 @@
+const { taskReport } = require('./lambda');
+const { uploadReport } = require('./s3');
+
+const logMsg = (message, params = {}) => {
+  const base = { type: 'BatonJobCompleted', message };
+  return JSON.stringify({ ...base, ...params });
+};
+
+const getTaskReport = async taskId => {
+  let err;
+  let res;
+  let data;
+
+  try {
+    const result = await taskReport({ type: 'taskReport', params: { taskId } });
+    const { Payload } = result;
+    res = Payload;
+  } catch (e) {
+    console.error(e);
+    err = e.message;
+  }
+
+  if (res && !err) {
+    const { data: d, error } = JSON.parse(res);
+    err = error;
+    data = d;
+  }
+
+  return { data, err };
+};
+
+class CompletedJob {
+  constructor(event) {
+    this.event = event;
+    this.props = this.defaultProps();
+  }
+
+  defaultProps() {
+    const { contentId, taskId } = this.event;
+    const [pk, sk] = contentId.split('|');
+    return {
+      pk, sk, contentId, taskId,
+    };
+  }
+
+  async getReport() {
+    const { taskId } = this.props;
+    console.log(logMsg('fetching task report', { taskId }));
+
+    const { data, err } = await getTaskReport(taskId);
+    this.report = data;
+
+    return err;
+  }
+
+  async uploadReport() {
+    const { taskId } = this.props;
+    const key = `${taskId}.xml`;
+    console.log(logMsg('uploading task report', { key }));
+
+    return uploadReport(key, this.report);
+  }
+
+  async process() {
+    const err = await this.getReport();
+
+    // TODO handle error, updated ddb as failed & send failed event
+    if (!err) {
+      await this.uploadReport();
+    }
+    console.log('err: ', err);
+  }
+}
+
+module.exports = {
+  CompletedJob,
+};
