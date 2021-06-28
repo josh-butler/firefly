1206078579968fbe353ba76ea33f7ef5c01acf67
josh-butler <scinewave@gmail.com>
2021-06-27 20:26:44 -0400

    add completed class

--- src/handlers/alerts-consumer.js
+++ src/handlers/alerts-consumer.js
@@ -1,3 +1,4 @@
+/* eslint-disable max-classes-per-file */
 const xml2js = require('xml2js');
 
 const { taskReport } = require('../util/lambda');
@@ -49,22 +50,48 @@ const getTaskReport = async taskId => {
   return { data, err };
 };
 
-const handleTaskCompleted = async alert => {
-  const { contentId, taskId } = alert;
-  console.log('taskId: ', taskId);
-
-  // const [pk, sk] = contentId.split('|');
-  // console.log('pk: ', pk);
-  // console.log('sk: ', sk);
-
-  const { data, err } = await getTaskReport(taskId);
-  if (data) {
-    const res = await uploadReport(`${taskId}.xml`, data);
-    console.log('res: ', res);
+class CompletedJob {
+  constructor(event) {
+    this.event = event;
+    this.props = this.defaultProps();
   }
-  // console.log('data: ', data);
-  // console.log('err: ', err);
-  return 1;
+
+  defaultProps() {
+    const { contentId, taskId } = this.event;
+    const [pk, sk] = contentId.split('|');
+    return {
+      pk, sk, contentId, taskId,
+    };
+  }
+
+  async getReport() {
+    const { taskId } = this.props;
+    const { data, err } = await getTaskReport(taskId);
+    this.report = data;
+    return err;
+  }
+
+  async uploadReport() {
+    const { taskId } = this.props;
+    return uploadReport(`${taskId}.xml`, this.report);
+  }
+
+  async process() {
+    const err = await this.getReport();
+
+    // TODO handle error, updated ddb as failed & send failed event
+    if (!err) {
+      const res = await this.uploadReport();
+      console.log('res: ', res);
+    }
+    console.log('err: ', err);
+    console.log('this.report: ', this.report);
+  }
+}
+
+const handleTaskCompleted = async alert => {
+  const job = new CompletedJob(alert);
+  return job.process();
 };
 
 const handleAlert = async alert => {
@@ -74,7 +101,6 @@ const handleAlert = async alert => {
   switch (eventCode) {
     case 'TaskCompleted':
       res = handleTaskCompleted(alert);
-      // res = {};
       console.log(`handle: ${eventCode}`);
       break;
     default:
